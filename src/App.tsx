0158d5ac3594ae17addf42db660209fa85814723
UlisesHdz <ulisesdejesusk9999@gmail.com>
2025-03-23 10:51:21 -0600

    hook useEffect mi practica basica ppara entender

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,27 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { Button } from "./components";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  // Función para obtener datos desde una API externa
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos/1"
-      );
-
-      if (!response.ok) {
-        throw new Error("Error al obtener los datos");
-      }
-
-      const jsonData = await response.json();
-
-      setData(jsonData);
-    } catch (error) {
-      setError(error as string);
-    } finally {
-      setLoading(false);
-    }
+  const [name, setName] = useState("Ulises");
+  const changeName = () => {
+    //setName('Jesus')
+    setName((prevName) => (prevName === "Ulises" ? "Jesus" : "Ulises"));
   };
 
-  /**
-   * useEffect se usa para manejar efectos secundarios en React, como:
-   * - Llamadas a APIs externas (fetch, axios, etc.)
-   * - Operaciones asíncronas (como peticiones HTTP)
-   * - Responder a cambios en props o estado
-   * - Integración con contexto global
-   * - Suscripción a eventos y limpieza de efectos al desmontar el componente
-   */
+  const llamadaAPi = () => {
+    console.log("llamada los daos");
+  };
 
-  //asycn con entidades externas
   useEffect(() => {
-    // 1- cuando se monta el componente
-    // 2- cada vez que se modifique uno de los valores de state
-    fetchData();
-  }, []);
-
-  // Mostrar mensaje de carga mientras se obtiene la información
-  if (loading) {
-    return <div>Cargando...</div>;
-  }
-
-  // Mostrar mensaje de error en caso de fallo
-  if (error) {
-    return <div>Error :{error}</div>;
-  }
+    console.log("se monto el componente");
+    llamadaAPi();
+  }, [name]);
 
   return (
     <>
-      {/* Muestra los datos obtenidos de la API en formato JSON */}
-      <div>{JSON.stringify(data)}</div>
+      <p>Hola {name}</p>
+      <Button label="Cambiar nombre" parentMethod={changeName} />
     </>
   );
 }
